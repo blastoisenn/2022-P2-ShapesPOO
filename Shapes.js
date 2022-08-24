8dcc7c4bd12bf1cebe4e368b096a683a1f81b7e9
n <nicolasduve44@gmail.com>
2022-08-24 20:21:19 -0300

    a

--- Shapes.js
+++ Shapes.js
@@ -57,7 +57,59 @@ class Triangle extends Shape {
     }
 }
 
-export {Circle, Rectangle, Square, Triangle};
+class Pentagon extends Shape {
+     constructor (l5, a5) {
+        super();
+        this.lado = l5;
+        this.apotema = a5;
+     }
+
+     perimeter(){
+        return this.lado * 5;
+     }
+
+     area() {
+        return ((this.lado*5)*this.apotema)/2
+     }
+}
+
+class Hexagon extends Shape {
+    constructor (l6, a6) {
+       super();
+       this.lado = l6;
+       this.apotema = a6;
+    }
+
+    perimeter(){
+       return this.lado * 6;
+    }
+
+    area() {
+       return ((this.lado*6)*this.apotema)/2
+    }
+}
+
+class Triangle2 extends Triangle {
+       constructor (a, b, c){
+        super();
+        this.lado1 = a;
+        this.lado2 = b;
+        this.lado3 = c;
+       
+       }
+        perimeter(){
+            return this.lado1+this.lado2+this.lado3;
+        }
+
+        angles(){
+            this.angulo1 = Math.acos((b**2+c**2-a**2)/(2*b*c));
+            this.angulo2 = Math.acos((a**2+c**2-b**2)/(2*a*c));
+            this.angulo3 = Math.acos((a**2+b**2-c**2)/(2*b*a));
+        }
+}
+
+
+export {Circle, Rectangle, Square, Triangle, Pentagon, Hexagon, Triangle2};
 
 
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {Circle, Rectangle, Square, Triangle} from './Shapes.js';
+import {Circle, Rectangle, Square, Triangle, Pentagon, Hexagon, Triangle2} from './Shapes.js';
 
 const triangle = new Triangle(10, 5);
 const triangle2 = new Triangle(9, 19);
@@ -13,3 +13,9 @@ console.log(`Area: ${rectangle.area()} - Perimeter: ${rectangle.perimeter()}`);
 
 const circle = new Circle(10);
 console.log(circle.perimeter());
+
+const pentagon = new Pentagon(5, 6);
+console.log(`Area: ${pentagon.area()} - Perimeter: ${pentagon.perimeter()}`);
+
+const hexagon = new Hexagon(5, 6);
+console.log(`Area: ${hexagon.area()} - Perimeter: ${hexagon.perimeter()}`);
